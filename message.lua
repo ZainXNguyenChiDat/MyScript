ec4047c42f13919f6842bf6dc876302819f0ac62
ZainXNguyenChiDat <112574925+ZainXNguyenChiDat@users.noreply.github.com>
2023-11-06 22:05:32 +0700

    Update message.lua

--- message.lua
+++ message.lua
@@ -8,6 +8,7 @@ local Window = Fluent:CreateWindow({
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Transparenct = false,
     Theme = "Darker",
     MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
