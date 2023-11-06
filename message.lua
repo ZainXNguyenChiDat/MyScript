10340f2e66e445a255c1c3361e9251644d2b222b
ZainXNguyenChiDat <112574925+ZainXNguyenChiDat@users.noreply.github.com>
2023-11-06 21:58:26 +0700

    Update message.lua

--- message.lua
+++ message.lua
@@ -34,46 +34,6 @@ local mouse = game.Players.LocalPlayer:GetMouse()
   end)
  end
 
- function autorejoin()
-    if not game:IsLoaded() then
-        game.Loaded:Wait()
-    end
-    
-    if isfile and readfile and writefile and delfile and isfile("autorj-lastimsg.rj") then
-        game.StarterGui:SetCore("SendNotification",{
-            Title = "Auto Rejoind!",
-            Text = "Message: \n"..readfile("autorj-lastimsg.rj"),
-            Icon = "",
-            Duration = 3
-        })
-        delfile("autorj-lastimsg.rj")
-    end
-    
-    game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(prompt)
-        if prompt.Name == "ErrorPrompt" and prompt:FindFirstChild('MessageArea') and prompt.MessageArea:FindFirstChild("ErrorFrame") then
-            spawn(function()
-                if writefile and readfile and isfile and delfile then
-                    writefile("autorj-lastimsg.rj", game.CoreGui.RobloxPromptGui.promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ErrorMessage.Text)
-                end
-                game.StarterGui:SetCore("SendNotification",{
-                    Title = "Auto Rejoining...",
-                    Text = "Rejoining...",
-                    Icon = "",
-                    Duration = math.huge
-                })
-                while wait() do 
-                  if #game.Players:GetPlayers() <= 1 then
-                       wait()
-                       game:GetService('TeleportService'):Teleport(game.PlaceId, game.Players.LocalPlayer)
-                   else
-                       game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
-                   end
-                end
-            end)
-        end
-    end
-
-
  function hop()
     local PlaceID = game.PlaceId
     local AllIDs = {}
@@ -300,18 +260,6 @@ Tabs.Travel:AddButton({
     end
 })
 --------------------------------
-local Toggle = Tabs.Others:AddToggle("MyToggle", {Title = "Auto Rejoin", Default = false })
-
-Toggle:OnChanged(function()
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/baechooYT/Just_Stop/main/autorj-forautoexec.lua"), true))()
-
-    print("Toggle changed:", Options.MyToggle.Value)
-
-end)
-
-Options.MyToggle:SetValue(false)
-
-
 Tabs.Others:AddButton({
     Title = "Rejoin",
     Callback = function()
