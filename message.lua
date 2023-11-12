4262ea29d5fa0b0caac890672b06c29e1c7af493
ZainXNguyenChiDat <112574925+ZainXNguyenChiDat@users.noreply.github.com>
2023-11-12 20:39:38 +0700

    Update message.lua

--- message.lua
+++ message.lua
@@ -121,19 +121,10 @@ end
 local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Toggle", Default = false })
 
 Toggle:OnChanged(function(vu)
-    _G.Auto_Ken = vu
     print("Toggle changed:", Options.MyToggle.Value)
+    AutoHaki()
 end)
-spawn(function()
-	while wait(2) do
-		pcall(function()
-			if _G.Auto_Ken then
-				game:GetService("ReplicatedStorage").Remotes.CommE:FireServer("Ken",true)
-				wait(7)
-			end
-		end)
-	end
-end)
+
 
 Options.MyToggle:SetValue(false)
 
